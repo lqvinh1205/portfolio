5bb192ed035665ddda2a001fbaee3139b0fc39e5
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-05-09 19:16:32 +0700

    deploy vercel

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL: "http://localhost:8000/api"
+    baseURL: "https://portfolio-lqvinh1205.herokuapp.com/api"
 })
 
 export default instance
